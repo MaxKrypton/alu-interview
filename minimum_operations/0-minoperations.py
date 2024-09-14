6035202e48b9685a7b1d12271aa84463832f5ee1
Gentil Quentin <q.rurangirw@alustudent.com>
2024-09-14 21:13:25 +0200

    All files

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script demonstrates how to use Python and mathematical concepts
+to solve a problem. In this scenario, we use prime factorization.
+"""
+
+
+def minOperations(n):
+    """Calculates the minimum number of operations needed to result
+    in exactly n 'H' characters in the file.
+    Args:
+        n (int): The target number of 'H' characters.
+    Returns:
+        int: The fewest number of operations needed"""
+    if n <= 1:
+        return 0
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    return operations
